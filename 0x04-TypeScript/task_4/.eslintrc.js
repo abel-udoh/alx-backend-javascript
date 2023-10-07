23aeac4b5c9653029cb1c28c9a8e8cdc98cec827
abel-udoh <abeludoh8@gmail.com>
2023-10-07 07:49:00 -0700

    task 4

--- 0x04-TypeScript/task_4/.eslintrc.js
+++ 0x04-TypeScript/task_4/.eslintrc.js
@@ -1,37 +1,12 @@
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  rules:  {
   },
-  plugins: ['jest'],
-  rules: {
-    'max-classes-per-file': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };
